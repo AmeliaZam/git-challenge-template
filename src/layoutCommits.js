acc4fa8daadeb0494a010ed0f618c278b879bba2
Tom Coleman <tom@thesnail.org>
2020-11-10 16:06:58 +1100

    Fix

--- src/layoutCommits.js
+++ src/layoutCommits.js
@@ -1,10 +1,10 @@
 // Attach a row+column to each commit in a sensible way so the history can be understood.
-// NOTE: the initial implelementation just puts them next to each other in a 4-column grid.
+// NOTE: the initial implementation just puts them next to each other in a 4-column grid.
 export function layoutCommits(commits) {
   const COLS = 4;
   return commits.map((commit, index) => ({
     ...commit,
     row: Math.floor(index / COLS),
-    column: COLS - (index % COLS),
+    column: index % COLS,
   }));
 }
